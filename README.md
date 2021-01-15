cf2135bce98339dde07edb0c88ff689a879f07f7
BryanGK <bryangordonkrauss@gmail.com>
2021-01-15 15:43:54 -0800

    Game now works, first to 5 wins, and put in reset button.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 This is part of my ongoing learning through The Odin Project.
 Thanks for putting this course together. Hopefully someday I'll use my skills learned here to start a career in web development.
+
+Jan 15 - I think i have the game completed with a rudamentary UI. I will comeback soon and make the UI better.
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function playRound(x, y) {
+function playRound(x, y) { // game algorithm (if you can call it that, yikes)
     if (x === "rock" && y === "scissors") {
         return "You win this round!";
     } else if (x === "paper" && y === "rock") {
@@ -21,12 +21,7 @@ function playRound(x, y) {
         return "Something's not right here."
 }
 
-function playerPlay() {
-    playerSelection = prompt("Select: Rock, Paper, Scissors").toLowerCase();
-    return playerSelection;
-}
-
-function computerPlay() {
+function computerPlay() { // computers guess
     computerVar = Math.random();
     if (computerVar < 0.333) {
         return "rock";
@@ -37,25 +32,92 @@ function computerPlay() {
     }
 }
 
-function playRock () {
-    y = computerPlay();
-    // Here's where you left off, next you were adding in the rock conditions
-}
-
-const btn_rock = document.querySelector('#rock');
+const btn_rock = document.querySelector('#rock'); //player guesses rock
 
 btn_rock.addEventListener('click', () => {
-    console.log("Rock");
+    if (win >= 5 || lose >= 5) {
+        return;
+    }
+    y = computerPlay();
+    x = "rock";
+    round = (playRound(x, y));
+    const compGuess = document.querySelector('comp');
+    comp.textContent = "Computers guess: " + y + ", Your guess: rock";
+    const output = document.querySelector('score');
+    score.textContent = "Result: " + round;
+    scoreRound(round);
 });
 
-const btn_paper = document.querySelector('#paper');
+const btn_paper = document.querySelector('#paper'); //player guesses paper
 
 btn_paper.addEventListener('click', () => {
-    console.log("Paper");
+    if (win >= 5 || lose >= 5) {
+        return;
+    }
+    y = computerPlay();
+    x = "paper"
+    round = (playRound(x, y));
+    const compGuess = document.querySelector('comp');
+    comp.textContent = "Computers guess: " + y + ", Your guess: paper";
+    const output = document.querySelector('score');
+    score.textContent = "Result: " + round;
+    scoreRound(round);
 });
 
-const btn_scissors = document.querySelector('#scissors');
+const btn_scissors = document.querySelector('#scissors'); //player guesses scissors
 
 btn_scissors.addEventListener('click', () => {
-    console.log("Scissors")
+    if (win >= 5 || lose >= 5) {
+        return;
+    }
+    y = computerPlay();
+    x = "scissors"
+    round = (playRound(x, y));
+    const compGuess = document.querySelector('comp');
+    comp.textContent = "Computers guess: " + y + ", Your guess: scissors";
+    const output = document.querySelector('score');
+    score.textContent = "Result: " + round;
+    scoreRound(round);
 });
+
+
+let win = 0;
+let lose = 0;
+
+function scoreRound(round) { // round scoring
+         if (round === "You win this round!") {
+        win++;
+        const outputWins = document.querySelector('wins');
+        wins.textContent = "Wins: " + win;
+        
+    } else if (round === "You lose.") {
+        lose++;
+        const outputLoses = document.querySelector('loses');
+        loses.textContent = "Loses: " + lose;
+    }
+    if (win >= 5) {
+        const youWin = document.querySelector('endGame');
+        endGame.textContent = "Congrats! You won this game!"
+    } else if (lose >= 5) {
+        const youLose = document.querySelector('endGame');
+        endGame.textContent = "Better luck next time."
+    }
+}
+const btnReset = document.querySelector('#reset'); // reset game
+    btnReset.addEventListener('click', () => {
+    resetGame();
+});
+ 
+function resetGame() { // reset game
+    const reset = document.querySelector('endGame');
+    const outputWins = document.querySelector('wins');
+    const outputLoses = document.querySelector('loses');
+    const output = document.querySelector('score');
+    score.textContent = "Result: ";
+    endGame.textContent = '';
+    wins.textContent = 'Wins: 0';
+    loses.textContent = 'Loses: 0';
+    win = 0;
+    lose = 0;
+}
+
